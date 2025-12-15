1e0698ddd45ec57716979a7e785dc03a0a3b49ef
Belal Salah <belal.salah259@gmail.com>
2025-12-15 22:06:11 +0200

    final version

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -6,9 +6,17 @@ public class playerMovement : MonoBehaviour
 {
   public float speed ;
   public float RotSpeed;
-  public bool gameOver;
+  public bool gameOver = false;
   public GameObject gameOverObj;
   public GameObject explosion;
+    
+    void Start() {
+        gameOver = false;
+        if(gameOverObj != null) {
+            gameOverObj.SetActive(false);
+        }
+    }
+    
     // Update is called once per frame
     void Update() {
         if(gameOver == true){
